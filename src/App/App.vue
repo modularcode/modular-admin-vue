56594462deec428f482dedde735b9334c47007d2
Henrikh Abajyan <h.abajyan@gmail.com>
2017-09-16 16:08:56 +0400

    Initial layout setup

--- src/App/App.vue
+++ src/App/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
+  <div
+    id="App"
+    class="
+      App
+    " >
 
-    THis is app component
+    <div class="HeaderContainer">
+      <AppHeader />
+    </div>
+    <div class="SidebarContainer">
+      <!-- {{> App/_common/Sidebar/Sidebar}} -->
+    </div>
+    <div class="SidebarOverlay" id="SidebarOverlay"></div>
+    <div class="ContentContainer">
+      <div class="ContentBackground"></div>
+      <div class="Content">
 
-    <AppHeader />
+      </div>
+    </div>
+
+    <div class="FooterContainer" >
+      <!-- {{> App/_common/Footer/Footer}} -->
+    </div>
+
+    <!-- {{> App/_common/Search/Search}} -->
 
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style lang="scss" src="./App.scss"></style>
 
 <script>
   import AppHeader from './_common/AppHeader/AppHeader';
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// External modules
+import 'bootstrap/scss/bootstrap-reboot.scss';
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import ModularAdmin from './main.vue';
 import router from './router';
+
 
 // Common modules
 import './_common';
