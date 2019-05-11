95d0167e690cfcda1907572e3775caac2c2ef4b1
Gevorg Harutyunyan <gevharut@gmail.com>
2019-05-12 01:35:19 +0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 
-Vue.config.productionTip = false;
+import './main.scss'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
